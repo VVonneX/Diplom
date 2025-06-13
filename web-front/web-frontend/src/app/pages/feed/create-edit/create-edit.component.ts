f6484c0a3d87af410e7c74de541678723d4d47e5
Mikhail Churikov <churikov-20@yandex.ru>
2025-06-13 19:46:16 +0300

    frontend: refactor main page

--- web-front/web-frontend/src/app/pages/feed/create-edit/create-edit.component.ts
+++ web-front/web-frontend/src/app/pages/feed/create-edit/create-edit.component.ts
@@ -9,6 +9,14 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class CreateEditComponent implements OnInit {
 
+  editorConfig = {
+    apiKey: 'zznsxh4kwnf6at2cmvi47bnc4ztgo7vqypzj347u95dsj7lz', // 🔑 Замените на реальный ключ
+    plugins: 'lists link image table',
+    toolbar: 'undo redo | bold italic | bullist numlist | link image table',
+    menubar: false,
+    height: 400,
+    content_css: 'https://cdn.tiny.cloud/1/ваш-api-ключ/tinymce/6.8.2/skins/content/default/content.min.css'
+  };
   constructor(public dialogRef: MatDialogRef<CreateEditComponent>, @Inject(MAT_DIALOG_DATA) public data: FeedModel) { }
 
   ngOnInit(): void {
